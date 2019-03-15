dff2dfd16a5581edf929dab7b790901c88a658e5
bedyan <artem.benditov@gmail.com>
2019-03-15 18:50:19 +0300

    started work on clint side

---
+++ src/components/MainPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class MainPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      full_url: '',
+      short_url: ''
+    }
+
+  }
+  render() {
+    return (
+      
+    )
+  }
+}
+
+export default MainPage;
